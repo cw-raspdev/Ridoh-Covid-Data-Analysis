02159ba238d9b83086acc5d5b70218b6b3d45c1c
chilseawang <chilseawang@chilseas-mbp.devices.brown.edu>
2021-03-01 15:20:36 -0500

    Made some changes. This now outputs a table that has 'double' data values except for percents, which it prints as 'string' data values.

--- CSV2Array.m
+++ CSV2Array.m
@@ -1,1 +1,10 @@
-A = readmatrix(testsheet1.csv)
+opts = detectImportOptions('testsheet1.csv');
+opts.VariableNamingRule = 'modify';
+disp(opts.VariableNames);
+opts = setvartype(opts,'double'); %assumes everything is a number
+opts = setvartype(opts, 4, 'string'); %takes out the specific columns which don't just containnumbers
+A = readtable('testsheet1.csv', opts);
+disp(A)
+%readtable seems to work for sets of mixed numeric data rather than
+%readmatrix. here's a forum link to reference for next time: 
+%https://www.mathworks.com/matlabcentral/answers/458100-difference-between-readmatrix-and-readtable
