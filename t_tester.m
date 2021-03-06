2b615729b7b770a5b777532af285637019674a90
chilseawang <chilseawang@chilseas-mbp.devices.brown.edu>
2021-03-06 15:43:02 -0500

    Fixed "lowest-p-value" display and solved a double-comparison issue where each two-town pair was being counted twice (each town was Sample 1 for one of the counts and Sample 2 for the other count)

--- t_tester.m
+++ t_tester.m
@@ -3,24 +3,23 @@ global CityNames
 tests = [];
 pvalues = [];
 CityDifference = [];
-for ii = 1:size(ProcessedSheet, 2)-1
-    for jj = 1:size(ProcessedSheet, 2)-1
+for ii = 1:size(ProcessedSheet, 2)-1 %Doesn't include "total" column in ProcessedSheet
+    for jj = 1:ii %these loops are setup so that there's no double-comparison
     %FIRST: Do T-Test
     Sample1 = ProcessedSheet(:, ii);
     Sample2 = ProcessedSheet(:, jj);
     [t, p] = ttest2(Sample1, Sample2);
-    tests = [tests, t];
+    tests = [tests; t];
     pvalues = [pvalues; p];
     %SECOND: Get Label for Samples of T-Test
-    CityDifference = [CityDifference, append(CityNames(jj), '-', CityNames(ii))];
+    CityDifference = [CityDifference, append(CityNames(ii), '-', CityNames(jj))];
     end
 end
 %% Display T-test and p-values
-TwosampleTtest = tests(:);
-x = array2table(TwosampleTtest, 'RowNames', CityDifference);
-y = table(x, pvalues);
+x = array2table(tests, 'RowNames', CityDifference);
+y = table(x, pvalues)
 %% Display lowest p-value
-RejectNull = nnz(TwosampleTtest)
-disp 'The p-value is the probability of obtaining results at least as extreme as the observed results of a statistical hypothesis test'
+RejectNull = nnz(tests(:))
 [B, I] = mink(pvalues, 5)
-lowestPs = [CityDifference(I(1:end)), B]
+CityDiff1 = CityDifference(I(1:end))
+lowestPs = [CityDiff1(:), B]
