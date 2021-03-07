457638c8596306ac3f148164a0acf1895077d814
chilseawang <chilseawang@chilseas-mbp.devices.brown.edu>
2021-03-07 16:47:38 -0500

    Fixed a dimension error, hopefully; verified that code ran.

--- scatterplot.m
+++ scatterplot.m
@@ -1,9 +1,41 @@
 %average cases vs median income 
-avg_cases = rot90(mean(DoubleVersion),3)
-
+Testsheet = detectImportOptions('MedianIncome.csv');
+Testsheet = setvartype(Testsheet,'string');
+StringVersion = readmatrix('MedianIncome.csv', Testsheet);
 Testsheet = setvartype(Testsheet,'double');
-MedianIncome = readmatrix('MedianIncome.csv', Testsheet);
-median_income = MedianIncome(2:end, 3)
-array2table(avg_cases) 
-array2table(median_income)
-joinAll = join(median_income,avg_cases,'type','rightouter','mergekeys',true)
+DoubleVersion1 = readmatrix('MedianIncome.csv', Testsheet);
+    % creates a double (number) matrix from which we can detect NaN values
+%% Using strtok to clean up nonstandard numerical data
+for ii = 1:size(DoubleVersion1, 1)
+    % DoubleVersion and StringVersion are the same values but diff. formats
+    for jj = 1:size(DoubleVersion1, 2)
+        if isnan(DoubleVersion1(ii, jj)) 
+        % gives all values of DoubleVersion that are "not a number"
+            Tester = StringVersion(ii, jj);
+            Tester = strtok(Tester, '%'); %chops off '%' from Tester value
+            Tester = strtok(Tester, '<'); %chops off '<' from Tester value
+            StringVersion(ii, jj) = Tester;
+        %Tester is only ever one number at a time
+        end
+    end
+end
+
+DoubleVersion1 = str2double(StringVersion);
+%misc = [1 1 1 1]; 
+%DoubleVersion1 = [misc; DoubleVersion1]
+global CategNames
+
+
+%%
+avg_cases = rot90(mean(ProcessedSheet),3);
+median_income = DoubleVersion1(3:end-1,3); 
+CategNames1 = rot90(CategNames,3);
+CategNames1(end)=[]; %removes 'Total' string from category names
+CategNames1(end) = []; %removes 'Unknown' string from category names
+%The line CategNames1(end) = []; can be used as many times as needed to remove
+%values from start/end of CategNames1. Replace end with 1 to remove values
+%from start.
+table1 = [CategNames1, median_income, avg_cases(1:end-2)]
+scatterdata = str2double( table1 );
+scatter(scatterdata(:,2),scatterdata(:,3))
+
