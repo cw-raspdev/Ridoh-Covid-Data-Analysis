8acff046feb650e8dbe391d79f34a82c81a05bbe
chilseawang <chilseawang@chilseas-mbp.devices.brown.edu>
2021-03-06 19:13:11 -0500

    I made CSV2Array.m a function that you can input file names into.

--- CSV2Array.m
+++ CSV2Array.m
@@ -1,10 +1,12 @@
+function [ProcessedSheet, ProcessedTable] = CSV2Array
+filename = input('Enter File Name with single quotes and extension: ');
 %% Creating double/string formats from .csv file
-Testsheet = detectImportOptions('RIDOHData.csv');
+Testsheet = detectImportOptions(filename);
 Testsheet = setvartype(Testsheet,'string');
-StringVersion = readmatrix('RIDOHData.csv', Testsheet);
+StringVersion = readmatrix(filename, Testsheet);
     % creates a string matrix that we can edit
 Testsheet = setvartype(Testsheet,'double');
-DoubleVersion = readmatrix('RIDOHData.csv', Testsheet);
+DoubleVersion = readmatrix(filename, Testsheet);
     % creates a double (number) matrix from which we can detect NaN values
 %% Using strtok to clean up nonstandard numerical data
 for ii = 1:size(DoubleVersion, 1)
@@ -20,25 +22,6 @@ for ii = 1:size(DoubleVersion, 1)
         end
     end
 end
-%disp(StringVersion)
-DoubleVersion = str2double(StringVersion);
-x = array2table(DoubleVersion);
-        
-%[A, B] = strtok(A, '%')
-%Testsheet = setvartype(Testsheet,'double'); %assumes everything is a number
-%readtable seems to work for sets of mixed numeric data rather than
-%readmatrix. here's a forum link to reference for next time: 
-%https://www.mathworks.com/matlabcentral/answers/458100-difference-between-readmatrix-and-readtable
-%advantage of NaN is that will be excluded from mean of calculations, and
-%not a zeros
-
-
-%disp(StringVersion);
-%%
-CategNames = StringVersion(3, 2:end);
-DoubleVersion = str2double(StringVersion(4:end, 2:end));
-x = array2table(DoubleVersion, 'VariableNames', CategNames)
-
 %% Converting, displaying, and globalizing
 global CategNames
 global ProcessedSheet
@@ -47,5 +30,6 @@ CategNames = StringVersion(3, 2:end);
     % for displaying info.
 DoubleVersion = str2double(StringVersion(4:end, 2:end));
     % Cropped out values in the StringVersion that would just turn to NaN
-x = array2table(DoubleVersion, 'VariableNames', CategNames)
+ProcessedTable = array2table(DoubleVersion, 'VariableNames', CategNames);
 ProcessedSheet = DoubleVersion; %% DO NOT EDIT LIGHTLY
+end
